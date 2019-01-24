c308b720c1bfdda2e77db92c20e71598fb3ff330
Colin Cotter <colin.cotter@imperial.ac.uk>
2019-01-24 07:41:37 +0000

    draft average code

--- averaged_sw.py
+++ averaged_sw.py
@@ -1,6 +1,7 @@
 from cheby_exp import *
 from firedrake import *
 
+#some domain, parameters and FS setup
 R = 6371220.
 H = Constant(5960.)
 
@@ -29,6 +30,7 @@ g = Constant(9.8)  # Gravitational constant
 
 c = sqrt(g*H)
 
+#Set up the exponential operator
 operator_in = Function(W)
 u_in, h_in = split(operator_in)
 
@@ -61,68 +63,28 @@ Prob = LinearVariationalProblem(a, F, operator_out)
 OperatorSolver = LinearVariationalSolver(Prob, solver_parameters=params)
 
 eigs = [0.003465, 0.007274, 0.014955]
+min_time_period = 2*pi/eigs(ref_level-3)
 hours = 6
-t = 60*60*hours
-L = eigs[ref_level-3]*t
-print("L",L)
+dT = 60*60*hours
+L = eigs[ref_level-3]*dT
+Mbar = int(3*dT/min_time_period)
 ncheb = 10000
 
 cheby = cheby_exp(OperatorSolver, operator_in, operator_out,
                   ncheb, tol=1.0e-6, L=L)
 
-x0 = Function(W)
-ux, hx = x0.split()
-hx.interpolate(exp((cx+cy+cz)/R)*cx*cy*cz/R**3)
-y0 = Function(W)
+#solvers for slow part
+USlow_in = Function(W) #value at previous timestep
+USlow_out = Function(W) #value at RK stage
 
-y0.assign(x0)
-uy, hy = y0.split()
-file0 = File('cheb.pvd')
-file0.write(uy, hy)
+u0, h0 = split(USlow_in)
+u1, h1 = split(USlow_in)
 
-print("doing cheby")
-cheby.apply(x0, y0, t)
-
-file0.write(uy, hy)
-
-y0.assign(x0)
-
-solver_in = Function(W)
-u_in, h_in = split(solver_in)
-solver_out = Function(W)
-
-dt = t/1000
-Dt = Constant(dt)
-
+#RHS for Forward Euler step
+gradperp = lambda f: perp(grad(f))
+n = FacetNormal(mesh)
 L = (
-    - inner(f*perp(u),v)*dx
-    +c*h*div(v)*dx
-    - c*div(u)*phi*dx
+    inner(v, u0)*dx + phi*h0*dx
+    + dT*inner(gradperp(inner(v,u)))*dx
+    - dT*inner(
 )
-
-a = inner(v,u)*dx + phi*h*dx - 0.5*Dt*L
-F = action(inner(v,u)*dx + phi*h*dx + 0.5*Dt*L, solver_in)
-
-assemble(F)
-
-params = {
-    'mat_type': 'matfree',
-    'ksp_type': 'preonly',
-    'pc_type': 'python',
-    'pc_python_type': 'firedrake.HybridizationPC',
-    'hybridization': {'ksp_type': 'preonly',
-                      'pc_type': 'lu'}}
-
-Prob = LinearVariationalProblem(a, F, solver_out)
-Solver = LinearVariationalSolver(Prob, solver_parameters=params)
-
-t0 = 0.
-solver_in.assign(x0)
-print("time integration")
-while(t0 < t + 0.5*dt):
-    print(t0)
-    t0 += dt
-    Solver.solve()
-    solver_in.assign(solver_out)
-y0.assign(solver_out)
-file0.write(uy, hy)
